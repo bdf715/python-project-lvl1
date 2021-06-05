428f44afa4e95f591b37b2dec9e0b6d00a39e748
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-06-05 15:28:27 +0300

    fix: split functions in brain_gcd_game.py

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -11,13 +11,20 @@ def gcd(num_min, num_max):
     if num_max % num_min == 0:
         return True
 
+def sort_tuple(a, b):
+    '''Request tuple (a, b)
+    Return tuple (min(a, b), max(a, b)
+    '''
+    if a > b:
+        (a, b) = (b, a)
+    return (a, b)
+
 
 def game_set():
     a = randint(1, 100)
     b = randint(1, 100)
     data_to_show = '{0} {1}'.format(str(a), str(b))
-    if a > b:
-        (a, b) = (b, a)
+    (a, b) = sort_tuple(a, b)    
     div = a
     while div > 0:
         if gcd(div, a) and gcd(div, b):
