e5d53571699843d8d79526b28b87f5d25759bcfb
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-09 13:10:47 +0300

    fix: move user input to cli

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-
 """Brain-games CLI."""
 
 import prompt
 
 
-def welcome_user():
-    """Ask username, return greetings string."""
-    name = prompt.string('May I have your name? ')
-    return 'Hello, {0}!'.format(name)
-
-
-if __name__ == '__main__':
-    print(welcome_user())
+def get_user_input(question):
+    """Ask user for data, return user input"""
+    user_input = prompt.string(question)
+    return user_input
--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,10 +1,11 @@
 '''Brain Games main logic module'''
 
-import prompt
+from brain_games.cli import get_user_input
 
 
 def game_greetings(game_module):
-    user_name = prompt.string('May I have your name? ')
+    user_question = 'May I have your name? '
+    user_name = get_user_input(user_question)
     user_hello = 'Hello, {0}!'.format(user_name)
     game_hello = game_module.welcome_string()
     print('{0} \n {1}'.format(user_hello, game_hello))
@@ -14,10 +15,12 @@ def game_greetings(game_module):
 def game_start(game_module):
     (user_name, user_hello, game_hello) = game_greetings(game_module)
     count = 0
-    while count < 3:
+    total_count = 3
+    while count < total_count:
         data, correct_answer = game_module.game_set()
         print('Question: ' + data)
-        user_answer = prompt.string('Your answer: ')
+        user_question = 'Your answer: '
+        user_answer = get_user_input(user_question)
         if user_answer == correct_answer:
             print('Correct!')
             count += 1
