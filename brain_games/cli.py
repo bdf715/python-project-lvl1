161a0c22e48f826960947cb25269d813ec930aeb
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-09 13:33:47 +0300

    fix: return welcome_user func on cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,3 +7,9 @@ def get_user_input(question):
     """Ask user for data, return user input"""
     user_input = prompt.string(question)
     return user_input
+
+
+def welcome_user():
+    """Ask username, return greetings string."""
+    name = prompt.string('May I have your name? ')
+    return 'Hello, {0}!'.format(name)
