be02f12437fe8cc7262db7f80227970137df0fc9
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-15 16:30:53 +0300

    fix: brain-prime

--- brain_games/games/brain_prime_game.py
+++ brain_games/games/brain_prime_game.py
@@ -6,13 +6,17 @@ from random import randint
 welcome_string = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_not_prime(n, div):
+    return True if n % div == 0 else False
+
+
 def game_set():
     n = randint(1, 100)
     data_to_show = str(n)
     correct_answer = 'yes'
     div = n - 1
     while div in range(2, n + 1):
-        if n % div == 0:
+        if is_not_prime(n, div):
             correct_answer = 'no'
             break
         else:
