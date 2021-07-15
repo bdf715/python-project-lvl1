1b66849e3a62692ad202040035ee53a36de663b8
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-15 13:46:39 +0300

    fix: brain-calc

--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -6,16 +6,20 @@ from random import randint, choice
 welcome_string = 'What is the result of the expression?'
 
 
-def game_set():
-    a = randint(1, 100)
-    b = randint(1, 100)
-    sign = choice(['+', '-', '*'])
-    data_to_show = '{0} {1} {2}'.format(str(a), sign, str(b))
+def get_correct_answer(a, sign, b):
     if sign == '+':
         result = a + b
     elif sign == '-':
         result = a - b
     elif sign == '*':
         result = a * b
-    correct_answer = str(result)
+    return result
+
+
+def game_set():
+    a = randint(1, 100)
+    b = randint(1, 100)
+    sign = choice(['+', '-', '*'])
+    data_to_show = '{0} {1} {2}'.format(a, sign, b)
+    correct_answer = str(get_correct_answer(a, sign, b))
     return (data_to_show, correct_answer)
