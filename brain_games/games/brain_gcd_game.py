472d375f4274d6a8c02bf137568bfb65f6e756bd
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-15 16:53:45 +0300

    fix:brain-gcd

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -6,12 +6,11 @@ from random import randint
 welcome_string = 'Find the greatest common divisor of given numbers.'
 
 
-def gcd(num_min, num_max):
-    if num_max % num_min == 0:
-        return True
+def get_gcd(num_min, num_max):
+    return True if num_max % num_min == 0 else False
 
 
-def sort_tuple(a, b):
+def get_sorted(a, b):
     '''Request tuple (a, b)
     Return tuple (min(a, b), max(a, b)
     '''
@@ -23,11 +22,11 @@ def sort_tuple(a, b):
 def game_set():
     a = randint(1, 100)
     b = randint(1, 100)
-    data_to_show = '{0} {1}'.format(str(a), str(b))
-    (a, b) = sort_tuple(a, b)
+    data_to_show = '{0} {1}'.format(a, b)
+    (a, b) = get_sorted(a, b)
     div = a
     while div > 0:
-        if gcd(div, a) and gcd(div, b):
+        if get_gcd(div, a) and get_gcd(div, b):
             correct_answer = str(div)
             break
         else:
