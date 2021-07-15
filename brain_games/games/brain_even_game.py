0a683a3ac4940cb21c961ba70d9b912a1352ee2e
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-15 13:47:38 +0300

    fix: brain_even

--- brain_games/games/brain_even_game.py
+++ brain_games/games/brain_even_game.py
@@ -6,11 +6,12 @@ from random import randint
 welcome_string = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return True if number % 2 == 0 else False
+
+
 def game_set():
     number = randint(1, 100)
     data_to_show = str(number)
-    if number % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    correct_answer = 'yes' if is_even(number) else 'no'
     return (data_to_show, correct_answer)
