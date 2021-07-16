f1f1ba85727dd358a72184a3e3b08c64b75d2757
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-16 12:54:36 +0300

    fix: add func in_prime in brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,19 +6,20 @@ from random import randint
 welcome_string = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def is_not_prime(n, div):
-    return True if n % div == 0 else False
+def is_prime(n):
+    result = True
+    div = n - 1
+    while div in range(2, n):
+        if n % div == 0:
+            result = False
+            break
+        else:
+            div -= 1
+    return result
 
 
 def game_set():
     n = randint(1, 100)
     data_to_show = str(n)
-    correct_answer = 'yes'
-    div = n - 1
-    while div in range(2, n + 1):
-        if is_not_prime(n, div):
-            correct_answer = 'no'
-            break
-        else:
-            div -= 1
+    correct_answer = 'yes' if is_prime(n) else 'no'
     return (data_to_show, correct_answer)
