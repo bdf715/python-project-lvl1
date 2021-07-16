0939ad72f7e93c95c7c1ab11a682569717348e6d
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-16 12:59:13 +0300

    fix: add global const OPERATIONS in brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -6,7 +6,10 @@ from random import randint, choice
 welcome_string = 'What is the result of the expression?'
 
 
-def get_correct_answer(a, sign, b):
+OPERATIONS = ['+', '-', '*']
+
+
+def get_calculation(a, sign, b):
     if sign == '+':
         result = a + b
     elif sign == '-':
@@ -19,7 +22,7 @@ def get_correct_answer(a, sign, b):
 def game_set():
     a = randint(1, 100)
     b = randint(1, 100)
-    sign = choice(['+', '-', '*'])
+    sign = choice(OPERATIONS)
     data_to_show = '{0} {1} {2}'.format(a, sign, b)
-    correct_answer = str(get_correct_answer(a, sign, b))
+    correct_answer = str(get_calculation(a, sign, b))
     return (data_to_show, correct_answer)
